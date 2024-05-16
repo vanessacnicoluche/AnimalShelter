45c76d4080dbd2731c391d08911c346175a6d3c1
Daidha Chávez <149783345+daichav@users.noreply.github.com>
2024-05-16 19:29:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('bottonadopt').onclick = function(){
+    alert('Your form was Send. Thank you for choosing to adopt and provide a loving home to one of our dogs! Please submit your adoption application below, and our team will review it promptly. We will contact you soon to discuss the next steps in welcoming your new furry friend into your family.');
+}
